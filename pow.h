eca0e3c8b1d0ac24c37dabed068aa40ed76a8f54
jwellbelove <github@wellbelove.co.uk>
2014-11-02 17:18:09 +0000

    Individual classes originally from maths.h

---
+++ pow.h
@@ -0,0 +1,67 @@
+///\file
+
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+
+Copyright(c) 2014 jwellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#ifndef __etl_pow__
+#define __etl_pow__
+
+#include <cstddef>
+
+///\defgroup pow pow
+/// A set of templates to generate compile time constants.<br>
+/// pow<N, POWER> : Calculates N to the power POWER.
+///\ingroup Maths
+
+namespace etl 
+{
+  //***************************************************************************
+  ///\ingroup pow
+  /// Calculates powers.
+  //***************************************************************************
+  template <const size_t N, const size_t POWER>
+  struct pow
+  {
+    enum value_type
+    {
+      value = N * pow<N, POWER - 1>::value
+    };
+  };
+
+  //***************************************************************************
+  // Specialisation for POWER == 0
+  //***************************************************************************
+  template <const size_t N>
+  struct pow<N, 0>
+  {
+    enum value_type
+    {
+      value = 1
+    };
+  };
+}
+
+#endif
