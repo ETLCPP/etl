f5caa87408727ed7fad3edb0bcec565fc09267d8
jwellbelove <github@wellbelove.co.uk>
2014-12-08 20:09:51 +0000

    Added checksum template

---
+++ checksum.h
@@ -0,0 +1,165 @@
+///\file
+
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+
+Copyright(c) 2014 jwellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#ifndef __ETL_CHECKSUM__
+#define __ETL_CHECKSUM__
+
+#include <stdint.h>
+
+#include "static_assert.h"
+#include "type_traits.h"
+#include "endian.h"
+
+///\defgroup checksum Checksum calculation
+///\ingroup maths
+
+namespace etl
+{
+  //***************************************************************************
+  /// Calculates the checksum.
+  ///\tparam TSum       The type used for the sum.
+  ///\tparam ENDIANNESS The endianness of the calculation for input types larger than uint8_t. Default = endian::little.
+  ///\ingroup checksum
+  //***************************************************************************
+  template <typename TSum, const int ENDIANNESS = endian::little>
+  class checksum
+  {
+  public:
+
+    STATIC_ASSERT(is_unsigned<TSum>::value, "Signed TSum template parameter");
+
+    typedef TSum value_type;
+
+    //*************************************************************************
+    /// Default constructor.
+    //*************************************************************************
+    checksum()
+    {
+      reset();
+    }
+
+    //*************************************************************************
+    /// Constructor from range.
+    /// \param begin Start of the range.
+    /// \param end   End of the range.
+    //*************************************************************************
+    template<typename TIterator>
+    checksum(TIterator begin, const TIterator end)
+    {
+      reset();
+      add(begin, end);
+    }
+
+    //*************************************************************************
+    /// Resets the CRC to the initial state.
+    //*************************************************************************
+    void reset()
+    {
+      sum = 0;
+    }
+
+    //*************************************************************************
+    /// \param value The value to add to the checksum.
+    //*************************************************************************
+    template<typename TValue>
+    void add(TValue value)
+    {
+      STATIC_ASSERT(is_integral<TValue>::value, "Non-integral parameter");
+
+      if (ENDIANNESS == endian::little)
+      {
+        for (int i = 0; i < sizeof(TValue); ++i)
+        {
+          add(uint8_t((value >> (i * 8)) & 0xFF));
+        }
+      }
+      else
+      {
+        for (int i = sizeof(TValue) - 1; i >= 0; --i)
+        {
+          add(uint8_t((value >> (i * 8)) & 0xFF));
+        }
+      }
+    }
+
+    //*************************************************************************
+    /// \param value The char to add to the checksum.
+    //*************************************************************************
+    void add(uint8_t value)
+    {
+      sum += value;
+    }
+
+    //*************************************************************************
+    /// \param begin Start of the range.
+    /// \param end   End of the range.
+    /// \return The checksum result.
+    //*************************************************************************
+    template<typename TIterator>
+    void add(TIterator begin, const TIterator end)
+    {
+      while (begin != end)
+      {
+        add(*begin++);
+      }
+    }
+
+    //*************************************************************************
+    /// Gets the checksum value.
+    //*************************************************************************
+    value_type value() const
+    {
+      return sum;
+    }
+
+    //*************************************************************************
+    /// \param value The value to add to the checksum.
+    //*************************************************************************
+    template<typename TValue>
+    checksum<TSum, ENDIANNESS>& operator +=(TValue value)
+    {
+      add(value);
+
+      return *this;
+    }
+
+    //*************************************************************************
+    /// Conversion operator to value_type.
+    //*************************************************************************
+    operator value_type () const
+    {
+      return sum;
+    }
+
+  private:
+
+    value_type sum;
+  };
+}
+
+#endif
