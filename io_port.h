2faf5b9ab49be26507bfb99d1ef68b9efcf947c2
jwellbelove <github@wellbelove.co.uk>
2015-03-15 16:31:05 +0000

    Added IO port helper template classes.

---
+++ io_port.h
@@ -0,0 +1,370 @@
+///\file
+
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+https://github.com/ETLCPP/etl
+
+Copyright(c) 2014 jwellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#ifndef __ETL_IO_PORT__
+#define __ETL_IO_PORT__
+
+///\defgroup io_port io port
+/// IO port access
+///\ingroup utilities
+
+#include <stdint.h>
+
+#include "nullptr.h"
+#include "parameter_type.h"
+
+namespace etl
+{
+  //***************************************************************************
+  /// Read write port.
+  //***************************************************************************
+  template <typename T, uintptr_t ADDRESS = 0>
+  class io_port_rw
+  {
+  private:
+
+    typedef volatile T* pointer_t;
+    typedef typename etl::parameter_type<T>::type parameter_t;
+
+  public:
+
+    /// Read.
+    operator T() volatile const
+    {
+      return *reinterpret_cast<pointer_t>(ADDRESS);
+    }
+
+    /// Read.
+    T value() volatile const
+    {
+      return *reinterpret_cast<pointer_t>(ADDRESS);
+    }
+
+    /// Write.
+    io_port_rw& operator =(parameter_t value)
+    {
+      *reinterpret_cast<pointer_t>(ADDRESS) = value;
+      return *this;
+    }
+  };
+
+  //***************************************************************************
+  /// Read only port.
+  //***************************************************************************
+  template <typename T, uintptr_t ADDRESS = 0>
+  class io_port_ro
+  {
+  private:
+
+    typedef volatile const T* pointer_t;
+    typedef typename etl::parameter_type<T>::type parameter_t;
+
+    /// Write disabled.
+    void operator =(parameter_t value);
+
+  public:
+
+    /// Read.
+    operator T() volatile const
+    {
+      return *reinterpret_cast<pointer_t>(ADDRESS);
+    }
+
+    /// Read.
+    T value() volatile const
+    {
+      return *reinterpret_cast<pointer_t>(ADDRESS);
+    }
+  };
+
+  //***************************************************************************
+  /// Write only port.
+  //***************************************************************************
+  template <typename T, uintptr_t ADDRESS = 0>
+  class io_port_wo
+  {
+  private:
+
+    typedef T* pointer_t;
+    typedef typename etl::parameter_type<T>::type parameter_t;
+
+    /// Read disabled.
+    operator T() volatile const;
+
+  public:
+
+    /// Write.
+    void operator =(parameter_t value)
+    {
+      *reinterpret_cast<pointer_t>(ADDRESS) = value;
+      return *this;
+    }
+  };
+
+  //***************************************************************************
+  /// Write only port with shadow register.
+  //***************************************************************************
+  template <typename T, uintptr_t ADDRESS = 0>
+  class io_port_wos
+  {
+  private:
+
+    typedef T* pointer_t;
+    typedef typename etl::parameter_type<T>::type parameter_t;
+
+  public:
+
+    /// Read.
+    operator T() const
+    {
+      return shadow_value;
+    }
+
+    /// Read.
+    T value() const
+    {
+      return shadow_value;
+    }
+
+    /// Write.
+    io_port_wos& operator =(parameter_t value)
+    {
+      shadow_value = value;
+      *reinterpret_cast<pointer_t>(ADDRESS) = shadow_value;
+      return *this;
+    }
+
+  private:
+
+    T shadow_value;
+  };
+
+  //***************************************************************************
+  /// Read write port.
+  /// Specialisation for dynamic addresses.
+  //***************************************************************************
+  template <typename T>
+  class io_port_rw<T, 0>
+  {
+  public:
+
+    typedef typename etl::parameter_type<T>::type parameter_t;
+
+    // Default constructor.
+    io_port_rw()
+      : address(nullptr)
+    {
+    }
+
+    // Constructor.
+    io_port_rw(uint8_t* address_)
+      : address(reinterpret_cast<volatile T*>(address_))
+    {
+    }
+
+    /// Set the IO port address.
+    void set_address(void* address_)
+    {
+      address = reinterpret_cast<volatile T*>(address_);
+    }
+
+    /// Read.
+    operator T() volatile const
+    {
+      return *address;
+    }
+
+    /// Read.
+    T value() volatile const
+    {
+      return *address;
+    }
+
+    /// Write.
+    io_port_rw& operator =(parameter_t value)
+    {
+      *address = value;
+      return *this;
+    }
+
+  private:
+
+    volatile T* address;
+  };
+
+  //***************************************************************************
+  /// Read only port.
+  /// Specialisation for dynamic addresses.
+  //***************************************************************************
+  template <typename T>
+  class io_port_ro<T, 0>
+  {
+  public:
+
+    // Default constructor.
+    io_port_ro()
+      : address(nullptr)
+    {
+    }
+
+    // Constructor.
+    io_port_ro(void* address_)
+      : address(reinterpret_cast<volatile const T*>(address_))
+    {
+    }
+
+    void set_address(void* address_)
+    {
+        address = reinterpret_cast<volatile const T*>(address_);
+    }
+
+    /// Read.
+    operator T() volatile const
+    {
+      return *address;
+    }
+
+    /// Read.
+    T value() volatile const
+    {
+      return *address;
+    }
+
+  private:
+
+    typedef typename etl::parameter_type<T>::type parameter_t;
+
+    /// Write disabled.
+    void operator =(parameter_t value);
+
+    volatile const T* address;
+  };
+
+  //***************************************************************************
+  /// Write only port.
+  /// Specialisation for dynamic addresses.
+  //***************************************************************************
+  template <typename T>
+  class io_port_wo<T, 0>
+  {
+  public:
+
+    typedef typename etl::parameter_type<T>::type parameter_t;
+
+    // Default constructor.
+    io_port_wo()
+      : address(nullptr)
+    {
+    }
+
+    // Constructor.
+    io_port_wo(void* address_)
+      : address(reinterpret_cast<T*>(address_))
+    {
+    }
+
+    /// Set the IO port address.
+    void set_address(void* address_)
+    {
+      address = reinterpret_cast<T*>(address_);
+    }
+
+    /// Write.
+    void operator =(parameter_t value)
+    {
+      *address = value;
+    }
+
+  private:
+
+    /// Read disabled.
+    operator T() volatile const;
+
+    T* address;
+  };
+
+  //***************************************************************************
+  /// Write only port with shadow register.
+  /// Specialisation for dynamic addresses.
+  //***************************************************************************
+  template <typename T>
+  class io_port_wos<T, 0>
+  {
+  private:
+
+    typedef typename etl::parameter_type<T>::type parameter_t;
+
+  public:
+
+    // Default constructor.
+    io_port_wos()
+      : address(nullptr)
+    {
+    }
+
+    // Constructor.
+    io_port_wos(void* address_)
+      : address(reinterpret_cast<T*>(address_))
+    {
+    }
+
+    /// Set the IO port address.
+    void set_address(void* address_)
+    {
+      address = reinterpret_cast<T*>(address_);
+    }
+
+    /// Read.
+    operator T() const
+    {
+      return shadow_value;
+    }
+
+    /// Read.
+    T value() const
+    {
+      return shadow_value;
+    }
+
+    /// Write.
+    io_port_wos& operator =(parameter_t value)
+    {
+      shadow_value = value;
+      *address     = shadow_value;
+      return *this;
+    }
+
+  private:
+
+    T  shadow_value;
+    T* address;
+  };
+}
+
+#endif
---
+++ test/test_io_port.cpp
@@ -0,0 +1,108 @@
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+https://github.com/ETLCPP/etl
+
+Copyright(c) 2014 jwellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#include <UnitTest++/UnitTest++.h>
+
+#include "../io_port.h"
+
+#include <stdint.h>
+
+template <uintptr_t ADDRESS>
+struct serial_port
+{
+  etl::io_port_ro<uint8_t,  ADDRESS>      rxdata;
+  etl::io_port_wo<uint8_t,  ADDRESS + 1>  txdata;
+  etl::io_port_rw<uint16_t, ADDRESS + 2>  control;
+  etl::io_port_ro<uint16_t, ADDRESS + 4>  status;
+  etl::io_port_wos<uint8_t, ADDRESS + 6> control2;
+};
+
+struct dynamic_serial_port
+{
+  dynamic_serial_port(uint8_t* base)
+    : rxdata(base),
+      txdata(base + 1),
+      control(base + 2),
+      status(base + 4),
+      control2(base + 6)
+  {
+  }
+
+  etl::io_port_ro<uint8_t>  rxdata;
+  etl::io_port_wo<uint8_t>  txdata;
+  etl::io_port_rw<uint16_t> control;
+  etl::io_port_ro<uint16_t> status;
+  etl::io_port_wos<uint8_t> control2;
+};
+
+namespace
+{
+  SUITE(test_alignment)
+  {
+    //*************************************************************************
+    TEST(test_io_port)
+    {
+      serial_port<0x1234> port;
+    }
+
+    //*************************************************************************
+    TEST(test_dynamic_io_port)
+    {
+      uint8_t memory[7];
+      memory[0] = 0x12;
+      memory[1] = 0x00;
+      memory[2] = 0x00;
+      memory[3] = 0x00;
+      memory[4] = 0xBC;
+      memory[5] = 0x9A;
+      memory[6] = 0x00;
+
+      dynamic_serial_port port(memory);
+
+      uint8_t rxdata = port.rxdata;
+      CHECK_EQUAL(memory[0], rxdata);
+      CHECK_EQUAL(memory[0], port.rxdata);
+
+      port.txdata = 0x34;
+      CHECK_EQUAL(0x34, memory[1]);
+
+      port.control = 0x5678; // Little endian.
+      CHECK_EQUAL(0x5678, memory[2] | (memory[3] << 8));
+
+      uint16_t status = port.status;
+      CHECK_EQUAL(0x9ABC, status);
+      CHECK_EQUAL(0x9ABC, port.status);
+
+      port.control2 = 0xDE;
+      CHECK_EQUAL(0xDE, memory[6]);
+
+      int control2 = port.control2;
+      CHECK_EQUAL(0xDE, control2);
+      CHECK_EQUAL(0xDE, port.control2);
+    }
+  };
+}
