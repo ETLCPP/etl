d8a690cf1bc3c3616d43f81d3a9df0e8a917b042
John Wellbelove <smartgit@wellbelove.co.uk>
2015-08-29 16:26:20 +0100

    Added Pearson hash

---
+++ pearson.h
@@ -0,0 +1,176 @@
+///\file
+
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+https://github.com/ETLCPP/etl
+
+Copyright(c) 2014 jwellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#ifndef __ETL_PEARSON__
+#define __ETL_PEARSON__
+
+#include <stdint.h>
+
+#include "static_assert.h"
+#include "type_traits.h"
+#include "endian.h"
+#include "ihash.h"
+#include "array.h"
+
+#if defined(COMPILER_KEIL)
+#pragma diag_suppress 1300 
+#endif
+
+///\defgroup pearson Pearson hash calculation
+///\ingroup pearson
+
+namespace etl
+{
+  //***************************************************************************
+  /// Pearson lookup table
+  /// \ingroup pearson
+  //***************************************************************************
+  extern const uint8_t PEARSON_LOOKUP[];
+
+  //***************************************************************************
+  /// Calculates a Pearson hash
+  ///\tparam HASH_LENGTH The number of elements in the hash.
+  ///\tparam ENDIANNESS  The endianness of the calculation for input types larger than uint8_t. Default = endian::little.
+  /// \ingroup pearson
+  //***************************************************************************
+  template <const size_t HASH_LENGTH, const int ENDIANNESS = endian::little>
+  class pearson : public etl::ihash
+  {
+  public:
+
+    typedef etl::array<uint8_t, HASH_LENGTH> value_type;
+
+    //*************************************************************************
+    /// Default constructor.
+    //*************************************************************************
+    pearson()
+      : first(true),
+        ihash(etl::endian(ENDIANNESS))
+    {
+      reset();
+    }
+
+    //*************************************************************************
+    /// Constructor from range.
+    /// \param begin Start of the range.
+    /// \param end   End of the range.
+    //*************************************************************************
+    template<typename TIterator>
+    pearson(TIterator begin, const TIterator end)
+      : first(true),
+        ihash(etl::endian(ENDIANNESS))
+    {
+      reset();
+      add(begin, end);
+    }
+
+    //*************************************************************************
+    /// Resets the hash to the initial state.
+    //*************************************************************************
+    void reset()
+    {
+      hash.fill(0);
+    }
+
+    //*************************************************************************
+    /// Adds a range.
+    /// \param begin
+    /// \param end
+    //*************************************************************************
+    template<typename TIterator>
+    void add(TIterator begin, const TIterator end)
+    {
+      ihash::add(begin, end);
+    }
+
+    //*************************************************************************
+    /// Adds a value.
+    /// \param value The value to add to the hash.
+    //*************************************************************************
+    template<typename TValue>
+    void add(TValue value)
+    {
+      ihash::add(value);
+    }
+
+    //*************************************************************************
+    /// \param value The char to add to the hash.
+    //*************************************************************************
+    void add(uint8_t value)
+    {
+      if (first)
+      {
+        for (size_t i = 0; i < HASH_SIZE; ++i)
+        {
+          hash[i] = PEARSON_LOOKUP[(uint32_t(value) + i) % 256];
+        }
+
+        first = false;
+      }
+      else
+      {
+        for (size_t i = 0; i < HASH_SIZE; ++i)
+        {
+          hash[i] = PEARSON_LOOKUP[hash[i] ^ value];
+        }
+      }
+    }
+
+    //*************************************************************************
+    /// Gets the hash value.
+    //*************************************************************************
+    value_type value() const
+    {
+      return hash;
+    }
+
+    //*************************************************************************
+    /// Conversion operator to value_type.
+    //*************************************************************************
+    operator value_type () const
+    {
+      return value();
+    }
+
+    //*************************************************************************
+    /// Gets the generic digest value.
+    //*************************************************************************
+    generic_digest digest()
+    {
+      return generic_digest(&hash[0], &hash[HASH_LENGTH]);
+    }
+
+  private:
+
+    bool first;
+    value_type hash;
+  };
+}
+
+#endif
---
+++ test/test_pearson.cpp
@@ -0,0 +1,152 @@
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+https://github.com/ETLCPP/etl
+
+Copyright(c) 2015 jwellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#include <UnitTest++/UnitTest++.h>
+
+#include <iterator>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <stdint.h>
+
+#include "../pearson.h"
+#include "../endian.h"
+
+const size_t HASH_SIZE = 8;
+typedef etl::pearson<HASH_SIZE>::value_type hash_t;
+
+//***************************************************************************
+/// Pearson lookup table
+/// \ingroup pearson
+//***************************************************************************
+namespace etl
+{
+  extern const uint8_t PEARSON_LOOKUP[];
+}
+
+//***************************************************************************
+// Comparison calculator based on Wikipedia example. https://en.wikipedia.org/wiki/Pearson_hashing
+//***************************************************************************
+template <typename TContainer>
+hash_t Pearson_Compare(const TContainer& data)
+{
+  hash_t hash;
+  hash.fill(0);
+
+  for (size_t j = 0; j < HASH_SIZE; ++j)
+  {
+    uint8_t subhash = etl::PEARSON_LOOKUP[(data[0] + j) % 256];
+
+    for (size_t i = 1; i < data.size(); ++i)
+    {
+      subhash = etl::PEARSON_LOOKUP[subhash ^ data[i]];
+    }
+
+    hash[j] = subhash;
+  }
+
+  return hash;
+}
+
+//***************************************************************************
+// Output stream for hash_t
+//***************************************************************************
+std::ostream& operator <<(std::ostream& os, const hash_t& hash)
+{
+  for (size_t i = 0; i < hash.size(); ++i)
+  {
+    os << int(hash[i]) << " ";
+  }
+
+  return os;
+}
+
+namespace
+{		
+  SUITE(test_pearson)
+  {
+    //*************************************************************************
+    TEST(test_pearson_constructor)
+    {
+      std::string data("123456789");
+
+      hash_t compare = Pearson_Compare(data);
+      hash_t hash    = etl::pearson<HASH_SIZE>(data.begin(), data.end());
+
+      CHECK_EQUAL(compare, hash);
+    }
+
+    //*************************************************************************
+    TEST(test_pearson_add_values)
+    {
+      std::string data("123456789");
+
+      etl::pearson<HASH_SIZE> pearson_calculator;
+
+      for (size_t i = 0; i < data.size(); ++i)
+      {
+        pearson_calculator += data[i];
+      }
+
+      hash_t compare = Pearson_Compare(data);
+      hash_t hash    = pearson_calculator;
+
+      CHECK_EQUAL(compare, hash);
+    }
+
+    //*************************************************************************
+    TEST(test_pearson_add_range)
+    {
+      std::string data("123456789");
+
+      etl::pearson<HASH_SIZE> pearson_calculator;
+
+      pearson_calculator.add(data.begin(), data.end());
+
+      hash_t compare = Pearson_Compare(data);
+      hash_t hash    = pearson_calculator.value();
+
+      CHECK_EQUAL(compare, hash);
+    }
+
+    //*************************************************************************
+    TEST(test_pearson_add_range_endian)
+    {
+      std::vector<uint8_t>  data1 = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+      std::vector<uint32_t> data2 = { 0x04030201, 0x08070605 };
+      std::vector<uint32_t> data3 = { 0x01020304, 0x05060708 };
+
+      hash_t hash1 = etl::pearson<HASH_SIZE, etl::endian::little>(data1.begin(), data1.end());
+      hash_t hash2 = etl::pearson<HASH_SIZE, etl::endian::little>(data2.begin(), data2.end());
+      CHECK_EQUAL(hash1, hash2);
+
+      hash_t hash3 = etl::pearson<HASH_SIZE, etl::endian::big>(data3.begin(), data3.end());
+      CHECK_EQUAL(hash1, hash3);
+    }
+  };
+}
+
