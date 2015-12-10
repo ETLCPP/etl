34e4351e851712efb48ce145d6c2d1487a229784
John Wellbelove <smartgit@wellbelove.co.uk>
2015-12-10 14:08:47 +0000

    Moved non-user headers to private sub-directory.

---
+++ private/list_base.h
@@ -0,0 +1,264 @@
+///\file
+
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+https://github.com/ETLCPP/etl
+
+Copyright(c) 2014 jwellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#ifndef __ETL_IN_ILIST_H__
+#error This header is a private element of etl::list & etl::ilist
+#endif
+
+#ifndef __ETL_LIST_BASE__
+#define __ETL_LIST_BASE__
+
+#include <stddef.h>
+#include "exception.h"
+
+namespace etl
+{
+  //***************************************************************************
+  /// Exception for the list.
+  ///\ingroup list
+  //***************************************************************************
+  class list_exception : public exception
+  {
+  public:
+
+    list_exception(const char* what)
+      : exception(what)
+    {
+    }
+  };
+
+  //***************************************************************************
+  /// Full exception for the list.
+  ///\ingroup list
+  //***************************************************************************
+  class list_full : public list_exception
+  {
+  public:
+
+    list_full()
+      : list_exception("list: full")
+    {
+    }
+  };
+
+  //***************************************************************************
+  /// Iterator exception for the list.
+  ///\ingroup list
+  //***************************************************************************
+  class list_iterator : public list_exception
+  {
+  public:
+
+    list_iterator()
+      : list_exception("list: iterator problem")
+    {
+    }
+  };
+
+  //***************************************************************************
+  /// The base class for all lists.
+  ///\ingroup list
+  //***************************************************************************
+  class list_base
+  {
+  public:
+
+    typedef size_t size_type; ///< The type used for determining the size of list.
+
+    //*************************************************************************
+    /// The node element in the list.
+    //*************************************************************************
+    struct Node
+    {
+      //***********************************************************************
+      /// Constructor
+      //***********************************************************************
+      Node()
+        : previous(nullptr),
+          next(nullptr)
+      {
+      }
+
+      //***********************************************************************
+      /// Reverses the previous & next pointers.
+      //***********************************************************************
+      void reverse()
+      {
+        std::swap(previous, next);
+      }
+
+      Node* previous;
+      Node* next;
+    };
+
+    //*************************************************************************
+    /// Reverses the list.
+    //*************************************************************************
+    void reverse()
+    {
+      if (is_trivial_list())
+      {
+        return;
+      }
+
+      Node* p_node = terminal_node.next;
+
+      while (p_node != &terminal_node)
+      {
+        p_node->reverse();
+        p_node = p_node->previous; // Now we've reversed it, we must go to the previous node.
+      }
+
+      // Terminal node.
+      p_node->reverse();
+    }
+
+    //*************************************************************************
+    /// Gets the size of the list.
+    //*************************************************************************
+    size_type size() const
+    {
+      return current_size;
+    }
+
+    //*************************************************************************
+    /// Gets the maximum possible size of the list.
+    //*************************************************************************
+    size_type max_size() const
+    {
+      return MAX_SIZE;
+    }
+
+    //*************************************************************************
+    /// Checks to see if the list is empty.
+    //*************************************************************************
+    bool empty() const
+    {
+      return current_size == 0;
+    }
+
+    //*************************************************************************
+    /// Checks to see if the list is full.
+    //*************************************************************************
+    bool full() const
+    {
+      return current_size == MAX_SIZE;
+    }
+
+    //*************************************************************************
+    /// Returns the remaining capacity.
+    ///\return The remaining capacity.
+    //*************************************************************************
+    size_t available() const
+    {
+      return max_size() - size();
+    }
+
+  protected:
+
+    //*************************************************************************
+    /// Get the head node.
+    //*************************************************************************
+    Node& get_head()
+    {
+      return *terminal_node.next;
+    }
+
+    //*************************************************************************
+    /// Get the head node.
+    //*************************************************************************
+    const Node& get_head() const
+    {
+      return *terminal_node.next;
+    }
+
+    //*************************************************************************
+    /// Get the tail node.
+    //*************************************************************************
+    Node& get_tail()
+    {
+      return *terminal_node.previous;
+    }
+
+    //*************************************************************************
+    /// Get the tail node.
+    //*************************************************************************
+    const Node& get_tail() const
+    {
+      return *terminal_node.previous;
+    }
+
+    //*************************************************************************
+    /// Insert a node before 'position'.
+    //*************************************************************************
+    void insert_node(Node& position, Node& node)
+    {
+      // Connect to the list.
+      join(*position.previous, node);
+      join(node, position);
+
+      // One more.
+      ++current_size;
+    }
+
+    //*************************************************************************
+    /// Is the list a trivial length?
+    //*************************************************************************
+    bool is_trivial_list() const
+    {
+      return (size() < 2);
+    }
+
+    //*************************************************************************
+    /// Join two nodes.
+    //*************************************************************************
+    void join(Node& left, Node& right)
+    {
+      left.next = &right;
+      right.previous = &left;
+    }
+
+    //*************************************************************************
+    /// The constructor that is called from derived classes.
+    //*************************************************************************
+    list_base(size_type max_size)
+      : current_size(0),
+        MAX_SIZE(max_size)
+
+    {
+    }
+
+    
+    Node            terminal_node; ///< The node that acts as the list start and end.
+    size_type       current_size;  ///< The number of the used nodes.
+    const size_type MAX_SIZE;      ///< The maximum size of the list.
+  };
+}
+
+#endif
