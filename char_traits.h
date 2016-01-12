07d5e63510d1475047a0d2c3a9049836e4fa1e37
jwellbelove <john.wellbelove@raymarine.com>
2016-01-12 11:00:24 +0000

    Updated with full set of types and functions.

--- char_traits.h
+++ char_traits.h
@@ -42,19 +42,50 @@ SOFTWARE.
 
 namespace etl
 {
-  template<typename T> struct char_traits;
+  template<typename T> struct char_traits_types;
   
-  //***************************************************************************
-  /// Specialisation for char.
-  //***************************************************************************
-  template<> struct char_traits<char>
+  template<> struct char_traits_types<char>
   {
     typedef char      char_type;
     typedef int       int_type;
     typedef long long off_type;
     typedef size_t    pos_type;
     typedef char      state_type;
-    
+  };
+  
+  template<> struct char_traits_types<wchar_t>
+  {
+    typedef wchar_t   char_type;
+    typedef wchar_t   int_type;
+    typedef long long off_type;
+    typedef size_t    pos_type;
+    typedef char      state_type;
+  };
+  
+  template<> struct char_traits_types<char16_t>
+  {
+    typedef char16_t       char_type;
+    typedef uint_least16_t int_type;
+    typedef long long      off_type;
+    typedef size_t         pos_type;
+    typedef char           state_type; 
+  };
+  
+  template<> struct char_traits_types<char32_t>
+  {
+    typedef char32_t       char_type;
+    typedef uint_least32_t int_type;
+    typedef long long      off_type;
+    typedef size_t         pos_type;
+    typedef char           state_type;  
+  };
+  
+  //***************************************************************************
+  /// Specialisation for char.
+  //***************************************************************************
+  template<typename T>
+  struct char_traits : public char_traits_types<T>
+  {   
     //*************************************************************************
     static bool eq(char_type a, char_type b)
     {
@@ -101,7 +132,7 @@ namespace etl
     }
     
     //*************************************************************************
-    static char_type* move(char_type* dest, const char_type* src, std::size_t count)
+    static char_type* move(char_type* dest, const char_type* src, size_t count)
     {
       if ((dest < src) || (dest > (src + count))
       {
@@ -113,224 +144,85 @@ namespace etl
                     std::reverse_iterator(src), 
                     std::reverse_iterator(dest + count));  
       }
+      
+      return dest;
     }
-  };
-  
-  //***************************************************************************
-  /// Specialisation for wchar_t
-  //***************************************************************************
-  template <> struct char_traits<wchar_t>
-  {
-    typedef wchar_t   char_type;
-    typedef wchar_t   int_type;
-    typedef long long off_type;
-    typedef size_t    pos_type;
-    typedef char      state_type;
-      
+    
     //*************************************************************************
-    static bool eq(char_type a, char_type b)
+    static char_type* copy(char_type* dest, const char_type* src, size_t count)
     {
-      return a == b;
+      etl::copy_n(src, src + count, dest);
+      
+      return dest;
     }
-      
+    
     //*************************************************************************
-    static bool lt(char_type a, char_type b)
-    {
-      return a < b;
-    }       
-      
-    //*************************************************************************
-    static size_t length(const wchar_t* str)
-    {
-      size_t count = 0;
-          
-      if (str != 0)
+    static int compare(const char_type* s1, const char_type* s2, size_t count)
+    {       
+      for (size_t i = 0; i < count; ++i)
       {
-        while (*str++ != 0)
+        if (*s1 < *s2)
         {
-          ++count;
+          return -1;
         }
-      }
-      
-      return count;
-    }      
-    
-    //*************************************************************************
-    static void assign(char_type& r, const char_type& c)
-    {
-      r = a;
-    }
-    
-    //*************************************************************************
-    static char_type* assign(char_type* p, size_t n, char_type c)
-    {
-      if (p != 0)
-      {
-        std::fill_n(p, n, c);
-      }
-      
-      return p;
-    }
-
-    //*************************************************************************
-    static char_type* move(char_type* dest, const char_type* src, std::size_t count)
-    {
-      if ((dest < src) || (dest > (src + count))
-      {
-        etl::copy_n(src, src + count, dest);
-      }
-      else
-      {
-        etl::copy_n(std::reverse_iterator(src + count), 
-                    std::reverse_iterator(src), 
-                    std::reverse_iterator(dest + count));   
-      }
-    }
-  };
-  
-  //***************************************************************************
-  /// Specialisation for char16_t.
-  //***************************************************************************
-  template <> struct char_traits<char16_t>
-  {  
-    typedef char16_t       char_type;
-    typedef uint_least16_t int_type;
-    typedef long long      off_type;
-    typedef size_t         pos_type;
-    typedef char           state_type;      
-      
-    //*************************************************************************
-    static bool eq(char_type a, char_type b)
-    {
-      return a == b;
-    }
-
-    //*************************************************************************
-    static bool lt(char_type a, char_type b)
-    {
-      return a < b;
-    }    
-    
-    //*************************************************************************
-    static size_t length(const char16_t* str)
-    {
-      size_t count = 0;
+          else if (*s1 > *s2)
+        {
+          return 1;
+        }
         
-      if (str != 0)
+        ++s1;
+        ++s2;
+      }
+        
+      return 0;
+    }
+    
+    //*************************************************************************
+    static const char_type* find(const char_type* p, size_t count, const char_type& ch)
+    {     
+      for (size_t i = 0; i < count; ++i)
       {
-        while (*str++ != 0)
+        if (*p == ch)
         {
-          ++count;
+          return p;
         }
-      }
-    
-      return count;
-    }
-    
-    //*************************************************************************
-    static void assign(char_type& r, const char_type& c)
-    {
-      r = a;
-    }
-  
-    //*************************************************************************
-    static char_type* assign(char_type* p, size_t n, char_type c)
-    {
-      if (p != 0)
-      {
-        std::fill_n(p, n, c);
-      }
-    
-      return p;
-    }   
-    
-    //*************************************************************************
-    static char_type* move(char_type* dest, const char_type* src, std::size_t count)
-    {
-      if ((dest < src) || (dest > (src + count))
-      {
-        etl::copy_n(src, src + count, dest);
-      }
-      else
-      {
-        etl::copy_n(std::reverse_iterator(src + count), 
-                    std::reverse_iterator(src), 
-                    std::reverse_iterator(dest + count));   
-      }
-    }    
-  }
-
-  //***************************************************************************
-  /// Specialisation for char32_t.
-  //***************************************************************************
-  template <> struct char_traits<char32_t>
-  {
-    typedef char16_t       char_type;
-    typedef uint_least32_t int_type;
-    typedef long long      off_type;
-    typedef size_t         pos_type;
-    typedef char           state_type;        
-      
-    //*************************************************************************
-    static bool eq(char_type a, char_type b)
-    {
-      return a == b;
-    }
-      
-    //*************************************************************************
-    static bool lt(char_type a, char_type b)
-    {
-      return a < b;
-    }     
-    
-    //*************************************************************************
-    static size_t length(const char32_t* str)
-    {
-      size_t count = 0;
-          
-      if (str != 0)
-      {
-        while (*str++ != 0)
-        {
-          ++count;
-        }
+        
+        ++p;
       }
       
-      return count;
+      return 0;
     }
     
     //*************************************************************************
-    static void assign(char_type& r, const char_type& c)
+    static char_type to_char_type(int_type c)
     {
-      r = a;
+      return static_cast<char_type>(c);
     }
     
     //*************************************************************************
-    static char_type* assign(char_type* p, size_t n, char_type c)
+    static int_type to_int_type(char_type c)
     {
-      if (p != 0)
-      {
-        std::fill_n(p, n, c);
-      }
-      
-      return p;
+      return static_cast<int_type>(c);
     }
     
     //*************************************************************************
-    static char_type* move(char_type* dest, const char_type* src, std::size_t count)
+    static bool eq_int_type(int_type c1, int_type c2)
     {
-      if ((dest < src) || (dest > (src + count))
-      {
-        etl::copy_n(src, src + count, dest);
-      }
-      else
-      {
-        etl::copy_n(std::reverse_iterator(src + count), 
-                    std::reverse_iterator(src), 
-                    std::reverse_iterator(dest + count));   
-      }
+      return (c1 == c2);
     }
-  }
+    
+    //*************************************************************************
+    static int_type eof()
+    {
+      return -1;
+    }
+    
+    //*************************************************************************
+    static int_type not_eof(int_type e)
+    {
+      return (e == eof()) ? eof() - 1 : e;
+    }
+  };
 }
 
 #endif
