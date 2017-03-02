f09b7ccc374de88c71a1e8816d33ea66e9a27d9e
John Wellbelove <gitkraken@wellbelove.co.uk>
2017-03-02 19:28:09 +0000

    Added 'typename'

--- src/iterator.h
+++ src/iterator.h
@@ -43,31 +43,31 @@ namespace etl
   template <typename T>
   struct is_input_iterator
   {
-    static const bool value = etl::is_same<std::iterator_traits<T>::iterator_category, std::input_iterator_tag>::value;
+    static const bool value = etl::is_same<typename std::iterator_traits<T>::iterator_category, std::input_iterator_tag>::value;
   };
 
   template <typename T>
   struct is_output_iterator
   {
-    static const bool value = etl::is_same<std::iterator_traits<T>::iterator_category, std::output_iterator_tag>::value;
+    static const bool value = etl::is_same<typename std::iterator_traits<T>::iterator_category, std::output_iterator_tag>::value;
   };
 
   template <typename T>
   struct is_forward_iterator
   {
-    static const bool value = etl::is_same<std::iterator_traits<T>::iterator_category, std::forward_iterator_tag>::value;
+    static const bool value = etl::is_same<typename std::iterator_traits<T>::iterator_category, std::forward_iterator_tag>::value;
   };
 
   template <typename T>
   struct is_bidirectional_iterator
   {
-    static const bool value = etl::is_same<std::iterator_traits<T>::iterator_category, std::bidirectional_iterator_tag>::value;
+    static const bool value = etl::is_same<typename std::iterator_traits<T>::iterator_category, std::bidirectional_iterator_tag>::value;
   };
 
   template <typename T>
   struct is_random_iterator
   {
-    static const bool value = etl::is_same<std::iterator_traits<T>::iterator_category, std::random_access_iterator_tag>::value;
+    static const bool value = etl::is_same<typename std::iterator_traits<T>::iterator_category, std::random_access_iterator_tag>::value;
   };
 
   template <typename T>
