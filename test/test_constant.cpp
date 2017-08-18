087c685f812657b14f8a3ae0fd5d33b67afb9086
John Wellbelove <smartgit@wellbelove.co.uk>
2017-08-18 12:58:25 +0100

    Added etl::constant helper class.

---
+++ test/test_constant.cpp
@@ -0,0 +1,65 @@
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+https://github.com/ETLCPP/etl
+https://www.etlcpp.com
+
+Copyright(c) 2017 jwellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#include "UnitTest++.h"
+
+#include "constant.h"
+#include "integral_limits.h"
+#include <type_traits>
+
+#undef max
+
+namespace
+{		
+  SUITE(test_constant)
+  {
+    //*************************************************************************
+    TEST(test_constants)
+    {
+      typedef etl::constant<int8_t,   etl::integral_limits<int8_t>::max>   C1;
+      typedef etl::constant<uint32_t, etl::integral_limits<uint32_t>::max> C2;
+      typedef etl::constant<int64_t,  etl::integral_limits<int64_t>::max>  C3;
+
+      C1 c1;
+      C2 c2;
+      C3 c3;
+
+      CHECK_EQUAL(etl::integral_limits<int8_t>::max, c1.value);
+      CHECK_EQUAL(etl::integral_limits<int8_t>::max, C1::value);
+      CHECK((std::is_same<int8_t, C1::value_type>::value));
+
+      CHECK_EQUAL(etl::integral_limits<uint32_t>::max, c2.value);
+      CHECK_EQUAL(etl::integral_limits<uint32_t>::max, C2::value);
+      CHECK((std::is_same<uint32_t, C2::value_type>::value));
+
+      CHECK_EQUAL(etl::integral_limits<int64_t>::max, c3.value);
+      CHECK_EQUAL(etl::integral_limits<int64_t>::max, C3::value);
+      CHECK((std::is_same<int64_t, C3::value_type>::value));
+    }
+  };
+}
