58bf40b55d46674c7fc833cdfd199ff165817484
John Wellbelove <github@wellbelove.co.uk>
2017-10-27 09:51:57 +0100

    Amalgamated ecl_timer_list into ecl_timer

--- src/c/ecl_timer.c
+++ src/c/ecl_timer.c
@@ -30,7 +30,174 @@ SOFTWARE.
 #include <assert.h>
 
 #include "ecl_timer.h"
-#include "ecl_timer_list.h"
+
+//*****************************************************************************
+// Internal timer list
+//*****************************************************************************
+
+static ecl_timer_id_t head;
+static ecl_timer_id_t tail;
+static ecl_timer_id_t current;
+
+static struct ecl_timer_config* ptimers;
+
+static void ecl_timer_list_init(struct ecl_timer_config* const ptimers_)
+{
+  ptimers = ptimers_;
+  head    = ECL_TIMER_NO_TIMER;
+  tail    = ECL_TIMER_NO_TIMER;
+  current = ECL_TIMER_NO_TIMER;
+}
+
+//*******************************
+static struct ecl_timer_config* ecl_timer_list_front()
+{
+  return &ptimers[head];
+}
+
+//*******************************
+static ecl_timer_id_t ecl_timer_list_begin()
+{
+  current = head;
+  return current;
+}
+
+//*******************************
+static ecl_timer_id_t ecl_timer_list_next(ecl_timer_id_t last)
+{
+  current = ptimers[last].next;
+  return current;
+}
+
+//*******************************
+static int ecl_timer_list_empty()
+{
+  return head == ECL_TIMER_NO_TIMER;
+}
+
+//*******************************
+// Inserts the timer at the correct delta position
+//*******************************
+static void ecl_timer_list_insert(ecl_timer_id_t id_)
+{
+  struct ecl_timer_config* ptimer = &ptimers[id_];
+
+  if (head == ECL_TIMER_NO_TIMER)
+  {
+    // No entries yet.
+    head = id_;
+    tail = id_;
+    ptimer->previous = ECL_TIMER_NO_TIMER;
+    ptimer->next     = ECL_TIMER_NO_TIMER;
+  }
+  else
+  {
+    // We already have entries.
+    ecl_timer_id_t test_id = ecl_timer_list_begin();
+
+    while (test_id != ECL_TIMER_NO_TIMER)
+    {
+      struct ecl_timer_config* ptest = &ptimers[test_id];
+
+      // Find the correct place to insert.
+      if (ptimer->delta <= ptest->delta)
+      {
+        if (ptest->id == head)
+        {
+          head = ptimer->id;
+        }
+
+        // Insert before ptest->
+        ptimer->previous = ptest->previous;
+        ptest->previous = ptimer->id;
+        ptimer->next = ptest->id;
+
+        // Adjust the next delta to compensate.
+        ptest->delta -= ptimer->delta;
+
+        if (ptimer->previous != ECL_TIMER_NO_TIMER)
+        {
+          ptimers[ptimer->previous].next = ptimer->id;
+        }
+        break;
+      }
+      else
+      {
+        ptimer->delta -= ptest->delta;
+      }
+
+      test_id = ecl_timer_list_next(test_id);
+    }
+
+    // Reached the end?
+    if (test_id == ECL_TIMER_NO_TIMER)
+    {
+      // Tag on to the tail.
+      ptimers[tail].next = ptimer->id;
+      ptimer->previous   = tail;
+      ptimer->next       = ECL_TIMER_NO_TIMER;
+      tail               = ptimer->id;
+    }
+  }
+}
+
+//*******************************
+static void ecl_timer_list_remove(ecl_timer_id_t id_, int has_expired)
+{
+  struct ecl_timer_config* ptimer = &ptimers[id_];
+
+  if (head == id_)
+  {
+    head = ptimer->next;
+  }
+  else
+  {
+    ptimers[ptimer->previous].next = ptimer->next;
+  }
+
+  if (tail == id_)
+  {
+    tail = ptimer->previous;
+  }
+  else
+  {
+    ptimers[ptimer->next].previous = ptimer->previous;
+  }
+
+  if (!has_expired)
+  {
+    // Adjust the next delta.
+    if (ptimer->next != ECL_TIMER_NO_TIMER)
+    {
+      ptimers[ptimer->next].delta += ptimer->delta;
+    }
+  }
+
+  ptimer->previous = ECL_TIMER_NO_TIMER;
+  ptimer->next     = ECL_TIMER_NO_TIMER;
+  ptimer->delta    = ECL_TIMER_INACTIVE;
+}
+
+//*******************************
+static void ecl_timer_list_clear()
+{
+  ecl_timer_id_t id = ecl_timer_list_begin();
+
+  while (id != ECL_TIMER_NO_TIMER)
+  {
+    struct ecl_timer_config* ptimer = &ptimers[id];
+    id = ecl_timer_list_next(id);
+    ptimer->next = ECL_TIMER_NO_TIMER;
+  }
+
+  head = ECL_TIMER_NO_TIMER;
+  tail = ECL_TIMER_NO_TIMER;
+  current = ECL_TIMER_NO_TIMER;
+}
+
+//*****************************************************************************
+// Timer Framework
+//*****************************************************************************
 
 //*******************************************
 /// Default initialisation.
