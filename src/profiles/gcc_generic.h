d4fdf9d657111eeeeebac0517363d0acd5f8c5bc
John Wellbelove <github@wellbelove.co.uk>
2018-01-27 15:40:52 +0000

    Added C++17 macros. The library doesn't currently support C++17.

--- src/profiles/gcc_generic.h
+++ src/profiles/gcc_generic.h
@@ -43,9 +43,11 @@ SOFTWARE.
 #ifdef __cplusplus
   #define ETL_CPP11_SUPPORTED                      (__cplusplus >= 201103L)
   #define ETL_CPP14_SUPPORTED                      (__cplusplus >= 201402L)
+  #define ETL_CPP17_SUPPORTED                      0
 #else
   #define ETL_CPP11_SUPPORTED                      0
   #define ETL_CPP14_SUPPORTED                      0
+  #define ETL_CPP17_SUPPORTED                      0
 #endif
 #define ETL_NO_NULLPTR_SUPPORT                     !ETL_CPP11_SUPPORTED
 #define ETL_NO_LARGE_CHAR_SUPPORT                  !ETL_CPP11_SUPPORTED
