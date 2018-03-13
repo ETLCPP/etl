1e27d10d0c4671c6559bd525fc4c9c04595d3e18
scott-eddy <eddy.scott88@gmail.com>
2018-03-13 08:14:01 -0400

    Installing and including headers but targets aren't imported

--- test/cmake/unit-test_external_project.cmake
+++ test/cmake/unit-test_external_project.cmake
@@ -3,9 +3,16 @@ include(ExternalProject)
 
 
 function (add_unittest_cpp)
+
+  set(UTPP_INSATLL_DIR "${CMAKE_CURRENT_BINARY_DIR}/unittest++/install")
+
   ExternalProject_Add(unittest-cpp
     GIT_REPOSITORY    https://github.com/unittest-cpp/unittest-cpp.git
     GIT_TAG           v2.0.0
+    SOURCE_DIR  "${CMAKE_CURRENT_BINARY_DIR}/unittest++/src"
+    INSTALL_DIR "${UTPP_INSATLL_DIR}"
+    CMAKE_ARGS  -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
   )
   
+  set(UTPP_INCLUDE_DIRS "${UTPP_INSATLL_DIR}/include/UnitTest++" PARENT_SCOPE)
 endfunction(add_unittest_cpp)
