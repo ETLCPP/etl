a0f2f8bf8eb9b58d17a7a05f629abdad06a223ce
John Wellbelove <github@wellbelove.co.uk>
2018-04-21 17:31:59 +0100

    Added null_type template

---
+++ include/etl/null_type.h
@@ -0,0 +1,50 @@
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+https://github.com/ETLCPP/etl
+https://www.etlcpp.com
+
+Copyright(c) 2018 jwellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#ifndef ETL_NULL_TYPE_INCLUDED
+#define ETL_NULL_TYPE_INCLUDED
+
+#undef ETL_FILE
+#define ETL_FILE "50"
+
+namespace etl
+{
+  template <size_t N>
+  class null_type
+  {
+  private:
+
+    null_type();
+    null_type(const null_type&);
+    null_type& operator=(const null_type&);
+  };
+}
+
+#undef ETL_FILE
+
+#endif
