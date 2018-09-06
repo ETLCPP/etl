01364bda14b3af678ecdc0972f7b7ce2d0ae4639
Prasenjit Sengupta <annius@users.noreply.github.com>
2018-09-06 13:16:01 -0700

    Add default C++17 profiles (#95)

--- include/etl/profiles/cpp17_no_stl.h
+++ include/etl/profiles/cpp17_no_stl.h
@@ -7,7 +7,7 @@ Embedded Template Library.
 https://github.com/ETLCPP/etl
 https://www.etlcpp.com
 
-Copyright(c) 2017 jwellbelove, scott-eddy
+Copyright(c) 2018 jwellbelove
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files(the "Software"), to deal
@@ -27,36 +27,23 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ******************************************************************************/
-#ifndef __ETL_PROFILE_H__
-#define __ETL_PROFILE_H__
-#ifdef PROFILE_MSVC
-  #include "msvc_x86.h"
-#elif PROFILE_GCC_GENERIC
-  #include "gcc_generic.h"
-#elif PROFILE_GCC_LINUX_X86
-  #include "gcc_linux_x86.h"
-#elif PROFILE_GCC_WINDOWS_X86
-  #include "gcc_windows_x86.h"
-#elif PROFILE_ARM_V5_GENERIC
-  #include "armv5.h"
-#elif PROFILE_ARM_V6_GENERIC
-  #include "armv6.h"
-#elif PROFILE_ARDUINO
-  #include "arduino_arm.h"
-#elif PROFILE_TICC
-  #include "ticc.h"
-#elif PROFILE_CPP03_GENERIC
-  #include "cpp03.h"
-#elif PROFILE_CPP11_GENERIC
-  #include "cpp11.h"
-#elif PROFILE_CPP14_GENERIC
-  #include "cpp14.h"
-#elif PROFILE_CPP17_GENERIC
-  #include "cpp17.h"
-#elif PROFILE_CUSTOM
-  #include "custom_profile.h"
-#else
-  #error Must provide a profile header file when buiding ETL. See https://www.etlcpp.com/setup.html
-#endif
 
-#endif // __ETL_PROFILE_H_
+#ifndef ETL_CPP17_NO_STL_INCLUDED
+#define ETL_CPP17_NO_STL_INCLUDED
+
+//*****************************************************************************
+// Generic C++17 without STL support
+//*****************************************************************************
+
+#define ETL_TARGET_DEVICE_GENERIC
+#define ETL_TARGET_OS_NONE
+#define ETL_COMPILER_GENERIC
+#define ETL_CPP11_SUPPORTED 1
+#define ETL_CPP14_SUPPORTED 1
+#define ETL_CPP17_SUPPORTED 1
+#define ETL_NO_NULLPTR_SUPPORT 0
+#define ETL_NO_LARGE_CHAR_SUPPORT 0
+#define ETL_CPP11_TYPE_TRAITS_IS_TRIVIAL_SUPPORTED 0
+#define ETL_NO_STL
+
+#endif
