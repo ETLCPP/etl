65c0de91108bbec1b3c957278a15fa36d0457dac
John Wellbelove <github@wellbelove.co.uk>
2018-09-18 18:39:35 +0100

    Merge remote-tracking branch 'origin/development'
    # Conflicts:
    #	include/etl/version.h
    #	support/Release notes.txt

--- include/etl/version.h
+++ include/etl/version.h
@@ -37,13 +37,13 @@ SOFTWARE.
 /// Definitions of the ETL version
 ///\ingroup utilities
 
-#define ETL_VERSION       "12.0.0"
-#define ETL_VERSION_W     L"12.0.0"
-#define ETL_VERSION_U16   u"12.0.0"
-#define ETL_VERSION_U32   U"12.0.0"
+#define ETL_VERSION       "12.0.1"
+#define ETL_VERSION_W     L"12.0.1"
+#define ETL_VERSION_U16   u"12.0.1"
+#define ETL_VERSION_U32   U"12.0.1"
 #define ETL_VERSION_MAJOR 12
 #define ETL_VERSION_MINOR  0
-#define ETL_VERSION_PATCH  0
+#define ETL_VERSION_PATCH  1
 #define ETL_VERSION_VALUE ((ETL_VERSION_MAJOR * 10000) + (ETL_VERSION_MINOR * 100) + ETL_VERSION_PATCH)
 
 #endif
