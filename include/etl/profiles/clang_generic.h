68e73f4d46fa35d5f4082d59940fe9202d2f1473
Tobias Müller <Tobias_Mueller@twam.info>
2019-07-24 20:17:34 +0200

    Add generic profile for Clang compiler

--- include/etl/profiles/clang_generic.h
+++ include/etl/profiles/clang_generic.h
@@ -7,7 +7,7 @@ Embedded Template Library.
 https://github.com/ETLCPP/etl
 https://www.etlcpp.com
 
-Copyright(c) 2017 jwellbelove, scott-eddy
+Copyright(c) 2017 jwellbelove
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files(the "Software"), to deal
@@ -27,38 +27,28 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ******************************************************************************/
-#ifndef __ETL_PROFILE_H__
-#define __ETL_PROFILE_H__
-#if defined(PROFILE_MSVC)
-  #include "msvc_x86.h"
-#elif defined(PROFILE_GCC_GENERIC)
-  #include "gcc_generic.h"
-#elif defined(PROFILE_GCC_LINUX_X86)
-  #include "gcc_linux_x86.h"
-#elif defined(PROFILE_GCC_WINDOWS_X86)
-  #include "gcc_windows_x86.h"
-#elif defined(PROFILE_CLANG_GENERIC)
-  #include "clang_generic.h"
-#elif defined(PROFILE_ARM_V5_GENERIC)
-  #include "armv5.h"
-#elif defined(PROFILE_ARM_V6_GENERIC)
-  #include "armv6.h"
-#elif defined(PROFILE_ARDUINO)
-  #include "arduino_arm.h"
-#elif defined(PROFILE_TICC)
-  #include "ticc.h"
-#elif defined(PROFILE_CPP03_GENERIC)
-  #include "cpp03.h"
-#elif defined(PROFILE_CPP11_GENERIC)
-  #include "cpp11.h"
-#elif defined(PROFILE_CPP14_GENERIC)
-  #include "cpp14.h"
-#elif defined(PROFILE_CPP17_GENERIC)
-  #include "cpp17.h"
-#elif defined(PROFILE_CUSTOM)
-  #include "custom_profile.h"
-#else
-  #error Must provide a profile header file when buiding ETL. See https://www.etlcpp.com/setup.html
-#endif
 
-#endif // __ETL_PROFILE_H_
+#ifndef ETL_CLANG_GENERIC_INCLUDED
+#define ETL_CLANG_GENERIC_INCLUDED
+
+//*****************************************************************************
+// GCC
+//*****************************************************************************
+
+#define ETL_TARGET_DEVICE_GENERIC
+#define ETL_TARGET_OS_NONE
+#define ETL_COMPILER_CLANG
+#ifdef __cplusplus
+  #define ETL_CPP11_SUPPORTED                      (__cplusplus >= 201103L)
+  #define ETL_CPP14_SUPPORTED                      (__cplusplus >= 201402L)
+  #define ETL_CPP17_SUPPORTED                      (__cplusplus >= 201703L)
+#else
+  #define ETL_CPP11_SUPPORTED                      0
+  #define ETL_CPP14_SUPPORTED                      0
+  #define ETL_CPP17_SUPPORTED                      0
+#endif
+#define ETL_NO_NULLPTR_SUPPORT                     !ETL_CPP11_SUPPORTED
+#define ETL_NO_LARGE_CHAR_SUPPORT                  !ETL_CPP11_SUPPORTED
+#define ETL_CPP11_TYPE_TRAITS_IS_TRIVIAL_SUPPORTED ETL_CPP14_SUPPORTED
+
+#endif
