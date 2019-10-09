5a8c65300486af5d7e8735eca6f0caa9ad744c21
John Wellbelove <github@wellbelove.co.uk>
2019-10-09 10:35:13 +0100

    Fixed strict aliasing bug for -O3 optimisation in etl::pool

--- include/etl/pool.h
+++ include/etl/pool.h
@@ -352,8 +352,9 @@ namespace etl
         // Initialise another one if necessary.
         if (items_initialised < MAX_SIZE)
         {
-          uintptr_t p = reinterpret_cast<uintptr_t>(p_buffer + (items_initialised * ITEM_SIZE));
-          *reinterpret_cast<uintptr_t*>(p) = p + ITEM_SIZE;
+          char* p = p_buffer + (items_initialised * ITEM_SIZE);
+          char* np = p + ITEM_SIZE;
+          *reinterpret_cast<char**>(p) = np;
           ++items_initialised;
         }
 
@@ -585,7 +586,7 @@ namespace etl
     // The pool element.
     union Element
     {
-      uintptr_t next;              ///< Pointer to the next free element.
+      char*     next;              ///< Pointer to the next free element.
       char      value[TYPE_SIZE_]; ///< Storage for value type.
       typename  etl::type_with_alignment<ALIGNMENT_>::type dummy; ///< Dummy item to get correct alignment.
     };
