e323f2267ffccd99aa7798e3a80c669c616f3fc6
John Wellbelove <github@wellbelove.co.uk>
2020-05-01 10:22:36 +0100

    Changed http://www.etlcpp.com to https://www.etlcpp.com

--- test/test_priority_queue.cpp
+++ test/test_priority_queue.cpp
@@ -3,7 +3,7 @@ The MIT License(MIT)
 
 Embedded Template Library.
 https://github.com/ETLCPP/etl
-http://www.etlcpp.com
+https://www.etlcpp.com
 
 Copyright(c) 2015 jwellbelove, rlindeman
 
--- test/test_vector_non_trivial.cpp
+++ test/test_vector_non_trivial.cpp
@@ -3,7 +3,7 @@
 //
 //Embedded Template Library.
 //https://github.com/ETLCPP/etl
-//http://www.etlcpp.com
+//https://www.etlcpp.com
 //
 //Copyright(c) 2014 jwellbelove
 //
