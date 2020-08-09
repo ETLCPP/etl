efbfc5c8ffe724171b3467fdd63827656c31818b
John Wellbelove <github@wellbelove.co.uk>
2020-08-09 17:01:45 +0100

    clang 9 compatibility

--- test/test_queue_spsc_locked_small.cpp
+++ test/test_queue_spsc_locked_small.cpp
@@ -34,7 +34,7 @@ SOFTWARE.
 #include <mutex>
 #include <vector>
 
-#if defined(ETL_COMPILER_MICROSOFT)
+#if defined(ETL_TARGET_OS_WINDOWS)
 #include <Windows.h>
 #endif
 
@@ -591,12 +591,13 @@ namespace
     }
 
     //*************************************************************************
-#if REALTIME_TEST && defined(ETL_COMPILER_MICROSOFT)
-  #if defined(ETL_TARGET_OS_WINDOWS) // Only Windows priority is currently supported
+#if REALTIME_TEST
+#if defined(ETL_TARGET_OS_WINDOWS) // Only Windows priority is currently supported
     #define RAISE_THREAD_PRIORITY  SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST)
     #define FIX_PROCESSOR_AFFINITY SetThreadAffinityMask(GetCurrentThread(), 1);
   #else
-    #error No thread priority modifier defined
+    #define RAISE_THREAD_PRIORITY
+    #define FIX_PROCESSOR_AFFINITY
   #endif
 
     size_t ticks = 0;
