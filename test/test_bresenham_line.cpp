e7d58dd52b63de05759cd0042347027da7f66a7b
John Wellbelove <github@wellbelove.co.uk>
2020-09-12 15:02:02 +0100

    Work around for Clang error in etl::bresenham_line tests.

--- test/test_bresenham_line.cpp
+++ test/test_bresenham_line.cpp
@@ -38,7 +38,7 @@ namespace
 
   using Point = etl::coordinate_2d<Value>;
 
-  std::ostream& operator << (std::ostream& os, const Point& point)
+  std::ostream& operator << (std::ostream& os, const etl::coordinate_2d<char>& point)
   {
     os << "(" << int(point.x) << "," << int(point.y) << ")";
     return os;
