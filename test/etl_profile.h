713031a514b9781fc1bfa08ecfc62ed3b82e9388
John Wellbelove <github@wellbelove.co.uk>
2020-09-29 12:09:49 +0100

    Conditionally disable template deduction guide tests

--- test/etl_profile.h
+++ test/etl_profile.h
@@ -103,6 +103,10 @@ SOFTWARE.
   #error THE UNIT TESTS REQUIRE C++17 SUPPORT
 #endif
 
+#if defined(ETL_COMPILER_GCC) && (ETL_COMPILER_VERSION < 8)
+  #define ETL_TEMPLATE_DEDUCTION_GUIDE_TESTS_DISABLED
+#endif
+
 #include "../include/etl/profiles/determine_development_os.h"
 
 #if defined(ETL_DEVELOPMENT_OS_WINDOWS)
