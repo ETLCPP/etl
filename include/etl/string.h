206c080866193f1a757710c21148fc6fb951e241
John Wellbelove <github@wellbelove.co.uk>
2021-03-10 18:38:02 +0000

    Fix default function templates for C++03

--- include/etl/string.h
+++ include/etl/string.h
@@ -147,7 +147,7 @@ namespace etl
     ///\param first The iterator to the first element.
     ///\param last  The iterator to the last element + 1.
     //*************************************************************************
-    template <typename TIterator>
+    template <typename TIterator, typename etl::enable_if<!etl::is_integral<TIterator>::value, int>::type = 0>
     string(TIterator first, TIterator last)
       : istring(reinterpret_cast<value_type*>(&buffer), MAX_SIZE)
     {
