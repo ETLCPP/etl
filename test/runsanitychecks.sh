9b84b19b5fdc9a2f12009cb10a0fc260f598e512
Bo Rydberg <2945606+bolry@users.noreply.github.com>
2021-03-15 11:19:18 +0100

    Feature/make sanity test also run with clang compiler (#353)
    * Cleanup runsanitychecks.sh & runtests.sh with shellcheck
    
    * Add executable writes on runsanitychecks.sh & runtests.sh
    
    for easier use of scripts.
    
    * Correct sanity tests to use gcc and clang compiler

--- test/runsanitychecks.sh
+++ test/runsanitychecks.sh
@@ -1,130 +1,118 @@
 #!/bin/sh
 cd sanity-check || exit 1
-echo "" > ../log.txt
+echo "" > log.txt
 
 ###############################################################################
 cd c++03 || exit 1
+cmake -E make_directory bgcc bclang
 { echo ""
 echo "-----------------------------------------------"
 echo " C++03"
 echo "-----------------------------------------------"
 
 echo "GCC - STL"; } >> ../log.txt
-gcc --version | grep gcc >> ../log.txt
-cmake --cmake-clean-cache -DNO_STL=OFF .
-make || echo "**** Failed ****" >> ../log.txt
+g++ --version | head --lines=1 >> ../log.txt
+CC=gcc CXX=g++ cmake -E chdir bgcc cmake --cmake-clean-cache -DNO_STL=OFF ..
+cmake --build bgcc || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "GCC - No STL"; } >> ../log.txt
-gcc --version | grep gcc >> ../log.txt
-cmake --cmake-clean-cache -DNO_STL=ON .
-make || echo "**** Failed ****" >> ../log.txt
+g++ --version | head --lines=1 >> ../log.txt
+CC=gcc CXX=g++ cmake -E chdir bgcc cmake --cmake-clean-cache -DNO_STL=ON ..
+cmake --build bgcc || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "Clang - STL"; } >> ../log.txt
-clang --version | grep clang >> ../log.txt
-export CC=clang
-export CXX=clang++
-cmake --cmake-clean-cache -DNO_STL=OFF .
-make || echo "**** Failed ****" >> ../log.txt
+clang++ --version | head --lines=1 >> ../log.txt
+CC=clang CXX=clang++ cmake -E chdir bclang cmake --cmake-clean-cache -DNO_STL=OFF ..
+cmake --build bclang || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "Clang - No STL"; } >> ../log.txt
-clang --version | grep clang >> ../log.txt
-export CC=clang
-export CXX=clang++
-cmake --cmake-clean-cache -DNO_STL=ON .
-make || echo "**** Failed ****" >> ../log.txt
+clang++ --version | head --lines=1 >> ../log.txt
+CC=clang CXX=clang++ cmake -E chdir bclang cmake --cmake-clean-cache -DNO_STL=ON ..
+cmake --build bclang || echo "**** Failed ****" >> ../log.txt
 
 ###############################################################################
 cd ../c++11 || exit 1
+cmake -E make_directory bgcc bclang
 { echo ""
 echo "-----------------------------------------------"
 echo " C++11"
 echo "-----------------------------------------------"
 
 echo "GCC - STL"; } >> ../log.txt
-gcc --version | grep gcc >> ../log.txt
-cmake --cmake-clean-cache -DNO_STL=OFF .
-make || echo "**** Failed ****" >> ../log.txt
+g++ --version | head --lines=1 >> ../log.txt
+CC=gcc CXX=g++ cmake -E chdir bgcc cmake --cmake-clean-cache -DNO_STL=OFF ..
+cmake --build bgcc || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "GCC - No STL"; } >> ../log.txt
-gcc --version | grep gcc >> ../log.txt
-cmake --cmake-clean-cache -DNO_STL=ON .
-make || echo "**** Failed ****" >> ../log.txt
+g++ --version | head --lines=1 >> ../log.txt
+CC=gcc CXX=g++ cmake -E chdir bgcc cmake --cmake-clean-cache -DNO_STL=ON ..
+cmake --build bgcc || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "Clang - STL"; } >> ../log.txt
-clang --version | grep clang >> ../log.txt
-export CC=clang
-export CXX=clang++
-cmake --cmake-clean-cache -DNO_STL=OFF .
-make || echo "**** Failed ****" >> ../log.txt
+clang++ --version | head --lines=1 >> ../log.txt
+CC=clang CXX=clang++ cmake -E chdir bclang cmake --cmake-clean-cache -DNO_STL=OFF ..
+cmake --build bclang || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "Clang - No STL"; } >> ../log.txt
-clang --version | grep clang >> ../log.txt
-export CC=clang
-export CXX=clang++
-cmake --cmake-clean-cache -DNO_STL=ON .
-make || echo "**** Failed ****" >> ../log.txt
+clang++ --version | head --lines=1 >> ../log.txt
+CC=clang CXX=clang++ cmake -E chdir bclang cmake --cmake-clean-cache -DNO_STL=ON ..
+cmake --build bclang || echo "**** Failed ****" >> ../log.txt
 
 ###############################################################################
 cd ../c++14 || exit 1
+cmake -E make_directory bgcc bclang
 { echo ""
 echo "-----------------------------------------------"
 echo " C++14"
 echo "-----------------------------------------------"
 
 echo " GCC - STL"; } >> ../log.txt
-gcc --version | grep gcc >> ../log.txt
-cmake --cmake-clean-cache -DNO_STL=OFF .
-make || echo "**** Failed ****" >> ../log.txt
+g++ --version | head --lines=1 >> ../log.txt
+CC=gcc CXX=g++ cmake -E chdir bgcc cmake --cmake-clean-cache -DNO_STL=OFF ..
+cmake --build bgcc || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "GCC - No STL"; } >> ../log.txt
-gcc --version | grep gcc >> ../log.txt
-cmake --cmake-clean-cache -DNO_STL=ON .
-make || echo "**** Failed ****" >> ../log.txt
+g++ --version | head --lines=1 >> ../log.txt
+CC=gcc CXX=g++ cmake -E chdir bgcc cmake --cmake-clean-cache -DNO_STL=ON ..
+cmake --build bgcc || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "Clang - STL"; } >> ../log.txt
-clang --version | grep clang >> ../log.txt
-export CC=clang
-export CXX=clang++
-cmake --cmake-clean-cache -DNO_STL=OFF .
-make || echo "**** Failed ****" >> ../log.txt
+clang++ --version | head --lines=1 >> ../log.txt
+CC=clang CXX=clang++ cmake -E chdir bclang cmake --cmake-clean-cache -DNO_STL=OFF ..
+cmake --build bclang || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "Clang - No STL"; } >> ../log.txt
-clang --version | grep clang >> ../log.txt
-export CC=clang
-export CXX=clang++
-cmake --cmake-clean-cache -DNO_STL=ON .
-make || echo "**** Failed ****" >> ../log.txt
+clang++ --version | head --lines=1 >> ../log.txt
+CC=clang CXX=clang++ cmake -E chdir bclang cmake --cmake-clean-cache -DNO_STL=ON ..
+cmake --build bclang || echo "**** Failed ****" >> ../log.txt
 
-###############################################################################
+  ###############################################################################
 cd ../c++17 || exit 1
+cmake -E make_directory bgcc bclang
 { echo ""
 echo "-----------------------------------------------"
 echo " C++17 "
 echo "-----------------------------------------------"
 
 echo "GCC - STL"; } >> ../log.txt
-gcc --version | grep gcc >> ../log.txt
-cmake --cmake-clean-cache -DNO_STL=OFF .
-make || echo "**** Failed ****" >> ../log.txt
+g++ --version | head --lines=1 >> ../log.txt
+CC=gcc CXX=g++ cmake -E chdir bgcc cmake --cmake-clean-cache -DNO_STL=OFF ..
+cmake --build bgcc || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "GCC - No STL"; } >> ../log.txt
-gcc --version | grep gcc >> ../log.txt
-cmake --cmake-clean-cache -DNO_STL=ON .
-make || echo "**** Failed ****" >> ../log.txt
+g++ --version | head --lines=1 >> ../log.txt
+CC=gcc CXX=g++ cmake -E chdir bgcc cmake --cmake-clean-cache -DNO_STL=ON ..
+cmake --build bgcc || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "Clang - STL"; } >> ../log.txt
-clang --version | grep clang >> ../log.txt
-export CC=clang
-export CXX=clang++
-cmake --cmake-clean-cache -DNO_STL=OFF .
-make || echo "**** Failed ****" >> ../log.txt
+clang++ --version | head --lines=1 >> ../log.txt
+CC=clang CXX=clang++ cmake -E chdir bclang cmake --cmake-clean-cache -DNO_STL=OFF ..
+cmake --build bclang || echo "**** Failed ****" >> ../log.txt
 
 { echo ""; echo "Clang - No STL"; } >> ../log.txt
-clang --version | grep clang >> ../log.txt
-export CC=clang
-export CXX=clang++
-cmake --cmake-clean-cache -DNO_STL=ON .
-make || echo "**** Failed ****" >> ../log.txt
+clang++ --version | head --lines=1 >> ../log.txt
+CC=clang CXX=clang++ cmake -E chdir bclang cmake --cmake-clean-cache -DNO_STL=ON ..
+cmake --build bclang || echo "**** Failed ****" >> ../log.txt
 
 { echo ""
 echo "-----------------------------------------------"
