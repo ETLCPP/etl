ab7fcbee645a90cd99f774cac726ceb13a469b49
John Wellbelove <john.wellbelove@asterconsulting.co.uk>
2021-05-25 23:06:15 +0100

    Added experimental uni_type

--- include/etl/experimental/mem_type.h
+++ include/etl/experimental/mem_type.h
@@ -1,15 +1,17 @@
 #pragma once
 template <size_t Size_>
-class uni_type
+class mem_type
 {
 public:
 
   static constexpr size_t Size = Size_;
 
-    //***********************************
-  template <typename T>
-  uni_type(const uni_type& other)
+  //***********************************
+  template <size_t Other_Size>
+  mem_type(const mem_type<Other_Size& other)
   {
+    ETL_STATIC_ASSERT(Size >= Other_Size, "Other size is too large");
+
     memcpy(buffer, other.buffer, Size_);
   }
 
@@ -29,15 +31,17 @@ public:
 
   //***********************************
   template <typename T>
-  operator T()
+  operator T() const
   {
     return *reinterpret_cast<T*>(buffer);
   }
 
   //***********************************
-  template <typename T>
-  uni_type& operator =(const uni_type& rhs)
+  template <size_t Other_Size>
+  mem_type& operator =(const mem_type<Other_Size>& rhs)
   {
+    ETL_STATIC_ASSERT(Size >= Other_Size, "RHS size is too large");
+
     memcpy(buffer, rhs.buffer, Size_);
 
     return *this;
@@ -49,28 +53,44 @@ public:
     return Size;
   }
 
+  //***********************************
+  constexpr char* data() const
+  {
+      return buffer;
+  }
+
 private:
 
   char buffer[Size]
 };
 
 template <size_t Size_>
-class uni_type_ptr
+class mem_type_ptr
 {
 public:
 
   static constexpr size_t Size = Size_;
 
   //***********************************
-  uni_type_ptr()
+  mem_type_ptr()
     : pbuffer(ETL_NULLPTR)
   {
   }
 
   //***********************************
-  uni_type_ptr(void* pbuffer_)
-    : pbuffer(reinterpret_cast<char*>(pbuffer))
+  mem_type_ptr(char* pbuffer_)
+    : pbuffer(pbuffer_)
   {
+  }
+
+  //***********************************
+  template <size_t Other_Size>
+  mem_type_ptr(const mem_type_ptr<Other_Size& other, char* pbuffer_)
+    : pbuffer(pbuffer_)
+  {
+      ETL_STATIC_ASSERT(Size >= Other_Size, "Other size is too large");
+
+      memcpy(buffer, other.buffer, Size_);
   }
 
   //***********************************
@@ -87,16 +107,34 @@ public:
     return *reinterpret_cast<T*>(pbuffer);
   }
 
+  //***********************************
   template <typename T>
-  operator T()
+  operator T() const
   {
     return *reinterpret_cast<T*>(pbuffer);
+  }
+
+  //***********************************
+  template <size_t Other_Size>
+  mem_type_ptr& operator =(const mem_type_ptr<Other_Size>& rhs)
+  {
+      ETL_STATIC_ASSERT(Size >= Other_Size, "RHS size is too large");
+
+      memcpy(pbuffer, rhs.pbuffer, Size_);
+
+      return *this;
   }
 
   //***********************************
   constexpr size_t size() const
   {
     return Size;
+  }
+
+  //***********************************
+  constexpr char* data() const
+  {
+      return pbuffer;
   }
 
 private:
