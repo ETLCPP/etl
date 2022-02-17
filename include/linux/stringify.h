a01b348ae9dc5825427789b7255cd96077842753
David Hebbeker <hebbeker@stoerk-tronic.com>
2022-02-17 18:25:01 +0100

    Add documentation.

--- include/linux/stringify.h
+++ include/linux/stringify.h
@@ -1,12 +1,16 @@
-#ifndef __LINUX_STRINGIFY_H
-#define __LINUX_STRINGIFY_H
+/** \file */
+#pragma once
 
-/* Indirect stringification.  Doing two levels allows the parameter to be a
- * macro itself.  For example, compile with -DFOO=bar, __stringify(FOO)
- * converts to "bar".
- */
-
+/** Helper macro for __stringify. */
 #define __stringify_1(x...) #x
-#define __stringify(x...) __stringify_1(x)
 
-#endif /* !__LINUX_STRINGIFY_H */
+/**
+ * Indirect stringification.
+ *
+ * Doing two levels allows the parameter to be a macro itself.
+ * For example, compile with `-DFOO=bar, __stringify(FOO)` converts to "bar".
+ *
+ * \param x will be converted to one string, including the spaces and commas between arguments.
+ *             For example `__stringify( a , b )` will be expanded to "a , b".
+ */
+#define __stringify(x...) __stringify_1(x)
