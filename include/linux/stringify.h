91840651a93d6c563fd0005f432c073aa40eeba7
David Hebbeker <hebbeker@stoerk-tronic.com>
2022-03-17 18:21:57 +0100

    Adjust names in order to satisfy naming convention.

--- include/linux/stringify.h
+++ include/linux/stringify.h
@@ -1,16 +1,16 @@
 /** \file */
 #pragma once
 
-/** Helper macro for __stringify. */
-#define __stringify_1(x...) #x
+/** Helper macro for STRINGIFY. */
+#define STRINGIFY_1(x...) #x
 
 /**
  * Indirect stringification.
  *
  * Doing two levels allows the parameter to be a macro itself.
- * For example, compile with `-DFOO=bar, __stringify(FOO)` converts to "bar".
+ * For example, compile with `-DFOO=bar`, `STRINGIFY(FOO)` converts to "bar".
  *
  * \param x will be converted to one string, including the spaces and commas between arguments.
- *             For example `__stringify( a , b )` will be expanded to "a , b".
+ *             For example `STRINGIFY( a , b )` will be expanded to "a , b".
  */
-#define __stringify(x...) __stringify_1(x)
+#define STRINGIFY(x...) STRINGIFY_1(x)
