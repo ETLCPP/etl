a8ecaf635b2c95ea7bd3e12a45d037b0b687af8c
John Wellbelove <jwellbelove@users.noreply.github.com>
2022-03-18 15:29:37 +0000

    Remove mem macros

--- include/etl/profiles/determine_builtin_support.h
+++ include/etl/profiles/determine_builtin_support.h
@@ -102,9 +102,6 @@ namespace etl
   {
     // Documentation: https://www.etlcpp.com/etl_traits.html
 
-    static ETL_CONSTANT bool using_builtin_memcmp                     = (ETL_USING_BUILTIN_MEMCMP == 1);
-    static ETL_CONSTANT bool using_builtin_memmove                    = (ETL_USING_BUILTIN_MEMMOVE == 1);
-    static ETL_CONSTANT bool using_builtin_memset                     = (ETL_USING_BUILTIN_MEMSET == 1);
     static ETL_CONSTANT bool using_builtin_is_assignable              = (ETL_USING_BUILTIN_IS_ASSIGNABLE == 1);
     static ETL_CONSTANT bool using_builtin_is_constructible           = (ETL_USING_BUILTIN_IS_CONSTRUCTIBLE == 1);
     static ETL_CONSTANT bool using_builtin_is_trivially_constructible = (ETL_USING_BUILTIN_IS_TRIVIALLY_CONSTRUCTIBLE == 1);
