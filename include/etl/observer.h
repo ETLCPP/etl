fa7f6dc9d7187fb246af920b3c3dcfe75bdb7bbf
John Wellbelove <jwellbelove@users.noreply.github.com>
2022-03-23 19:59:30 +0000

    Fixed move parameter in notify_observers

--- include/etl/observer.h
+++ include/etl/observer.h
@@ -232,31 +232,9 @@ namespace etl
       return observer_list.size();
     }
 
-#if ETL_USING_CPP11 && !defined(ETL_OBSERVER_FORCE_CPP03_IMPLEMENTATION)
     //*****************************************************************
     /// Notify all of the observers, sending them the notification.
-    ///\tparam TNotification the notification type.
-    ///\param n The notification.
-    //*****************************************************************
-    template <typename TNotification>
-    void notify_observers(TNotification&& n)
-    {
-      typename Observer_List::iterator i_observer_item = observer_list.begin();
-
-      while (i_observer_item != observer_list.end())
-      {
-        if (i_observer_item->enabled)
-        {
-          i_observer_item->p_observer->notification(etl::forward<TNotification>(n));
-        }
-
-        ++i_observer_item;
-      }
-    }
-#else
-    //*****************************************************************
-    /// Notify all of the observers, sending them the notification.
-    ///\tparam TNotification the notification type.
+    ///\tparam TNotification The notification type.
     ///\param n The notification.
     //*****************************************************************
     template <typename TNotification>
@@ -274,7 +252,6 @@ namespace etl
         ++i_observer_item;
       }
     }
-#endif
 
   protected:
 
