7ff5f4d04cfa8f7a34135cecccfeff3ec94bacf6
Robin Mueller <robin.mueller.m@gmail.com>
2022-03-29 14:02:17 +0200

    get version from git tag now

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -7,3 +7,19 @@ function(determine_version VER_FILE_NAME)
     )
     message(STATUS "Determined ETL version ${ETL_VERSION} from version.txt file")
 endfunction()
+
+function(determine_version_with_git)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/GetGitRevisionDescription.cmake)
+    git_describe(VERSION)
+    # Parse the version information into pieces.
+    string(REGEX REPLACE "^([0-9]+)\\..*" "\\1" VERSION_MAJOR "${VERSION}")
+    string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${VERSION}")
+    string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${VERSION}")
+    string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" VERSION_SHA1 "${VERSION}")
+    set(ETL_VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
+
+    set(ETL_VERSION ${ETL_VERSION} CACHE STRING
+        "ETL version determined from version.txt"
+    )
+    message(STATUS "Determined ETL version ${ETL_VERSION} from the git tag")
+endfunction()
