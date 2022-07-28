ec98d646970a1a22e9b229119cba92600b0e2398
John Wellbelove <jwellbelove@users.noreply.github.com>
2022-07-28 11:38:20 +0100

    Updated version info

--- include/etl/bip_buffer_spsc_atomic.h
+++ include/etl/bip_buffer_spsc_atomic.h
@@ -51,6 +51,7 @@ SOFTWARE.
 #include "utility.h"
 #include "error_handler.h"
 #include "span.h"
+#include "file_error_numbers.h"
 
 #if ETL_HAS_ATOMIC
 
@@ -77,7 +78,7 @@ namespace etl
   public:
 
     bip_buffer_reserve_invalid(string_type file_name_, numeric_type line_number_)
-      : bip_buffer_exception("bip_buffer:reserve", file_name_, line_number_)
+      : bip_buffer_exception(ETL_ERROR_TEXT("bip_buffer:reserve", ETL_BIP_BUFFER_SPSC_ATOMIC_FILE_ID"A"), file_name_, line_number_)
     {
     }
   };
@@ -421,7 +422,7 @@ namespace etl
     // Reserves an optimal memory area for writing. The buffer will only wrap
     // around if the available forward space is less than min_reserve_size.
     //*************************************************************************
-    span<T> write_reserve_optimal(size_type min_reserve_size = 1)
+    span<T> write_reserve_optimal(size_type min_reserve_size = 1U)
     {
         size_type reserve_size = numeric_limits<size_type>::max();
         size_type windex = get_write_reserve(&reserve_size, min_reserve_size);
