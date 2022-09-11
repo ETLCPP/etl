89d17dd7732fdb254945cdefe09aaa2fc2817772
John Wellbelove <jwellbelove@users.noreply.github.com>
2022-09-11 20:50:56 +0100

    Attempt to fix clang CI error

--- test/test_unordered_map.cpp
+++ test/test_unordered_map.cpp
@@ -417,7 +417,7 @@ namespace
 
       other_data = data;
 
-      bool isEqual = etl::equal(data.begin(),
+      bool isEqual = std::equal(data.begin(),
                                 data.end(),
                                 other_data.begin(),
                                 std::equal_to<>{});
