79ff8b4c85c0449081aa0ed3b3dfd860de7524ac
Drew Rife <darife@jlg.com>
2024-03-13 09:20:53 -0400

    test: or assignment constexpr

--- test/test_type_def.cpp
+++ test/test_type_def.cpp
@@ -321,6 +321,23 @@ namespace
     }
 
     //*************************************************************************
+    TEST(test_operator_or_assignment_constexpr)
+    {
+      class __type_t__;
+      typedef etl::type_def<__type_t__, uint32_t> type_t;
+
+      constexpr auto or_assignment = []() {
+        type_t t(0x5A3DUL);
+        return t |= 2;
+      };
+
+      uint32_t i = 0x5A3DUL;
+      constexpr type_t t = or_assignment();
+
+      CHECK_EQUAL(i|=2, uint32_t(t));
+    }
+
+    //*************************************************************************
     TEST(test_comparisons)
     {
       class __type_t__;
