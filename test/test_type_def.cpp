b6ac7f07c1f6b058d90594677b94f1c192da212d
Drew Rife <darife@jlg.com>
2024-03-13 09:25:43 -0400

    test: operator modules assignment

--- test/test_type_def.cpp
+++ test/test_type_def.cpp
@@ -371,7 +371,7 @@ namespace
       CHECK_EQUAL(i<<=2, uint32_t(t));
     }
 
-        //*************************************************************************
+    //*************************************************************************
     TEST(test_operator_right_shift_assignment_constexpr)
     {
       class __type_t__;
@@ -386,6 +386,23 @@ namespace
       constexpr type_t t = right_shift_assignment();
 
       CHECK_EQUAL(i>>=2, uint32_t(t));
+    }
+
+    //*************************************************************************
+    TEST(test_operator_modulus_assignment_constexpr)
+    {
+      class __type_t__;
+      typedef etl::type_def<__type_t__, uint32_t> type_t;
+
+      constexpr auto modulus_assignment = []() {
+        type_t t(0x5A3DUL);
+        return t %= 2;
+      };
+
+      uint32_t i = 0x5A3DUL;
+      constexpr type_t t = modulus_assignment();
+
+      CHECK_EQUAL(i%=2, uint32_t(t));
     }
 
     //*************************************************************************
