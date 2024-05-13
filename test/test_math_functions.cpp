8528ed2b80e0fbc72f44ff4e864c787ab7d30451
John Wellbelove <john.wellbelove@etlcpp.com>
2024-05-13 09:08:02 +0100

    Added GCD and LCM algorithms

--- test/test_math_functions.cpp
+++ test/test_math_functions.cpp
@@ -35,6 +35,8 @@ SOFTWARE.
 #include "etl/sqrt.h"
 #include "etl/permutations.h"
 #include "etl/combinations.h"
+#include "etl/gcd.h"
+#include "etl/lcm.h"
 
 namespace
 {
@@ -456,6 +458,46 @@ namespace
       CHECK_EQUAL((combinations(12,  8)), (actual = etl::combinations<12,  8>::value));
       CHECK_EQUAL((combinations(13,  9)), (actual = etl::combinations<13,  9>::value));
       CHECK_EQUAL((combinations(14, 10)), (actual = etl::combinations<14, 10>::value));
+    }
+
+    //*************************************************************************
+    TEST(test_gdc_for_positive_integers)
+    {
+      CHECK_EQUAL(1, etl::gcd( 3, 5,  7));
+      CHECK_EQUAL(2, etl::gcd( 4, 6,  8));
+      CHECK_EQUAL(3, etl::gcd( 9, 6,  3));
+      CHECK_EQUAL(4, etl::gcd( 8, 4, 12));
+      CHECK_EQUAL(5, etl::gcd(10, 5, 15));
+    }
+
+    //*************************************************************************
+    TEST(test_gdc_for_negative_integers)
+    {
+      CHECK_EQUAL(1, etl::gcd( -3, -5, -7));
+      CHECK_EQUAL(2, etl::gcd( -4, -6, -8));
+      CHECK_EQUAL(3, etl::gcd( -9, -6, -3));
+      CHECK_EQUAL(4, etl::gcd( -8, -4, -12));
+      CHECK_EQUAL(5, etl::gcd(-10, -5, -15));
+    }
+
+    //*************************************************************************
+    TEST(test_lcm_for_positive_integers) 
+    {
+      CHECK_EQUAL(12, etl::lcm(3, 4,  6));
+      CHECK_EQUAL(30, etl::lcm(3, 5, 10));
+      CHECK_EQUAL(20, etl::lcm(2, 5,  4));
+      CHECK_EQUAL(0,  etl::lcm(0, 5,  7));
+      CHECK_EQUAL(5,  etl::lcm(5, 5,  5));
+    }
+
+    //*************************************************************************
+    TEST(test_lcm_for_negative_integers)
+    {
+      CHECK_EQUAL(12, etl::lcm(-3, -4,  -6));
+      CHECK_EQUAL(30, etl::lcm(-3, -5, -10));
+      CHECK_EQUAL(20, etl::lcm(-2, -5,  -4));
+      CHECK_EQUAL(0,  etl::lcm(-0, -5,  -7));
+      CHECK_EQUAL(5,  etl::lcm(-5, -5,  -5));
     }
   };
 }
