f4a3e36705dac26f2d8e65bacbea9703b9bc4f8f
Roland Reichwein <Roland.Reichwein@bmw.de>
2024-12-29 21:54:25 +0100

    Add contains() and contains_node() to etl::intrusive_forward_list

--- include/etl/intrusive_forward_list.h
+++ include/etl/intrusive_forward_list.h
@@ -253,6 +253,30 @@ namespace etl
       return current_size;
     }
 
+    //*************************************************************************
+    /// Detects existence of specified node in list.
+    ///\param search_link The node to find in list
+    //*************************************************************************
+    bool contains_node(const link_type& search_link) const
+    {
+      const link_type* p_link = start.etl_next;
+
+      while (p_link != ETL_NULLPTR)
+      {
+        if (&search_link == p_link)
+        {
+          return true;
+        }
+
+        if (p_link != ETL_NULLPTR)
+        {
+          p_link = p_link->link_type::etl_next;
+        }
+      }
+
+      return false;
+    }
+
   protected:
 
     link_type start;             ///< The link pointer that acts as the intrusive_forward_list start.
@@ -1165,6 +1189,29 @@ namespace etl
 
         other.initialise();
       }
+    }
+
+    //*************************************************************************
+    /// Detects existence of specified value in list.
+    ///\param value The value to find in list
+    //*************************************************************************
+    bool contains(const_reference value) const
+    {
+      const_iterator i_item = begin();
+
+      while (i_item != end())
+      {
+        if (*i_item == value)
+        {
+          return true;
+        }
+        else
+        {
+          ++i_item;
+        }
+      }
+
+      return false;
     }
 
   private:
