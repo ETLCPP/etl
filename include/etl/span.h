adeb90224188d633a8417adad1eb7716caf79b0b
rolandreichweinbmw <Roland.Reichwein@bmw.de>
2025-01-04 14:33:05 +0100

    Fix span fixed extent empty (#1007)

--- include/etl/span.h
+++ include/etl/span.h
@@ -238,7 +238,7 @@ namespace etl
     //*************************************************************************
     ETL_NODISCARD ETL_CONSTEXPR bool empty() const ETL_NOEXCEPT
     {
-      return false;
+      return Extent == 0;
     }
 
     //*************************************************************************
