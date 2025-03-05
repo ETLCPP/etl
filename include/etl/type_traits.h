de17a4ffe8097de10eb4c78903651b6ac5762ec4
Chiang, Yi <yi.chiang@bmw.de>
2025-03-05 11:03:53 +0100

    Support etl::underlying_type with compiler builtin

--- include/etl/type_traits.h
+++ include/etl/type_traits.h
@@ -2295,6 +2295,41 @@ typedef integral_constant<bool, true>  true_type;
   using type_identity_t = typename type_identity<T>::type;
 #endif
 
+#if ETL_USING_BUILTIN_UNDERLYING_TYPE
+namespace private_type_traits
+{
+  template <typename T, bool = is_enum<T>::value>
+  struct __underlying_type_impl;
+
+  template <typename T>
+  struct __underlying_type_impl<T, false>
+  {
+  };
+
+  template <typename T>
+  struct __underlying_type_impl<T, true>
+  {
+    using type = __underlying_type(T);
+  };
+}
+
+template <typename T>
+struct underlying_type : private_type_traits::__underlying_type_impl<T, is_enum<T>::value>
+{
+};
+
+#if ETL_USING_CPP11
+template <typename T>
+using underlying_type_t = typename underlying_type<T>::type;
+
+template <typename T>
+ETL_CONSTEXPR underlying_type_t<T> to_underlying(T val) ETL_NOEXCEPT
+{
+  return static_cast<underlying_type_t<T>>(val);
+}
+#endif
+#endif
+
 #if ETL_USING_CPP11
   //*********************************************
   // has_duplicates
