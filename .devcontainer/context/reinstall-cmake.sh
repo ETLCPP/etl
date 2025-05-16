8262510e6fb10d6701b171eeeff123644c99fef3
William Sciaroni <wsciaroni@protonmail.com>
2025-05-16 00:54:17 +0000

    Remove comment

--- .devcontainer/context/reinstall-cmake.sh
+++ .devcontainer/context/reinstall-cmake.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-#-------------------------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
-#-------------------------------------------------------------------------------------------------------------
-#
+
 # For more detailed debugging, uncomment the next line
 # set -x
 
