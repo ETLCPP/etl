d17c0297ec0202fcbd04fd3b4b7250f1df14abb3
Roland Reichwein <Roland.Reichwein@bmw.de>
2026-03-31 17:36:54 +0200

    Factor out generated code (#1369)
    Previously, generator code under include/etl/generators needed to be
    synchronized with the generated files under include/etl explicitly.
    Leading to errors in the CI checks if forgotten.
    
    This change separates out the generated parts to cpp03 specific
    snippets. This way, most code under include/etl is now directly editable,
    with only some pre-C++11 code separated out to
    include/etl/generators/*_cpp03_generator.h which still needs to be
    re-generated on respective changes. But in many cases, this is not necessary.
    
    Co-authored-by: John Wellbelove <jwellbelove@users.noreply.github.com>

--- include/etl/generators/generate_message_router.bat
+++ include/etl/generators/generate_message_router.bat
@@ -1,1 +1,1 @@
-python3 -m cogapp -d -e -o../message_router.h -DHandlers=16 message_router_generator.h 
+python3 -m cogapp -d -e -o../private/message_router_cpp03.h -DHandlers=16 message_router_cpp03_generator.h
--- include/etl/generators/generate_smallest.bat
+++ include/etl/generators/generate_smallest.bat
@@ -1,1 +1,1 @@
-python3 -m cogapp -d -e -o../smallest.h -DNTypes=16 smallest_generator.h
+python3 -m cogapp -d -e -o../private/smallest_cpp03.h -DNTypes=16 smallest_cpp03_generator.h
