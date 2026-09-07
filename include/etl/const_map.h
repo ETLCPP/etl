aaa742b9a565a8622b6f7ea91a443fb6601f7a48
Roland Reichwein <Roland.Reichwein@bmw.de>
2026-09-07 13:21:05 +0200

    Fix false positive warning from GCC 16

--- include/etl/const_map.h
+++ include/etl/const_map.h
@@ -505,6 +505,7 @@ namespace etl
     static_assert((etl::is_default_constructible<key_type>::value), "key_type must be default constructible");
     static_assert((etl::is_default_constructible<mapped_type>::value), "mapped_type must be default constructible");
 
+  #include "private/diagnostic_uninitialized_push.h"
     //*************************************************************************
     ///\brief Construct a const_map from a variadic list of elements.
     /// Static asserts if the elements are not constructible into
@@ -512,6 +513,9 @@ namespace etl
     /// Static asserts if the number of elements is greater than the capacity of
     /// the const_map.
     //*************************************************************************
+    // The base class is passed the address of 'element_list' before that member has been
+    // initialised. The base class only stores the address and never reads through it during
+    // construction, so the compiler's 'may be used uninitialized' warning is a false positive.
     template <typename... TElements>
     ETL_CONSTEXPR14 explicit const_map(TElements&&... elements) ETL_NOEXCEPT
       : iconst_map<TKey, TMapped, TKeyCompare>(element_list, sizeof...(elements), Size)
@@ -521,6 +525,7 @@ namespace etl
                     "All elements must be constructible into value_type");
       static_assert(sizeof...(elements) <= Size, "Number of elements exceeds capacity");
     }
+  #include "private/diagnostic_pop.h"
 
   private:
 
