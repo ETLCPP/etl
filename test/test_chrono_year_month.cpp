725c05df87371b87b547482e2c9e798278beba8d
Hossein <37206671+HosseinSagha@users.noreply.github.com>
2026-09-12 19:22:39 +1000

    Exclude operator <=> from chrono when STL not available (#1602)
    * Exclude operator <=> from chrono when STL not available
    
    * apply the fix to tests
    
    * chore: rerun pipeline

--- test/test_chrono_year_month.cpp
+++ test/test_chrono_year_month.cpp
@@ -89,7 +89,7 @@ namespace
       CHECK_FALSE(ym.ok()); // Invalid year_month
     }
 
-#if ETL_USING_CPP20
+#if ETL_USING_CPP20 && ETL_USING_STL
     //*************************************************************************
     TEST(test_year_month_spaceship_operator)
     {
